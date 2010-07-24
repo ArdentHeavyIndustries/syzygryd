b39f03e2102917d714da0dc77fe107afac275d29
Rich Fromm <noreply@ardentheavyindustries.com>
2010-07-24 01:45:10 +0000

    add menus, hook up preferences
    git-svn-id: http://www.ardentheavyindustries.com/svn/syzygryd/trunk@459 1c6c7da2-4a7c-0410-9345-fc53533f7dce

--- android/modulator/src/syzygryd/modulator/Modulator.java
+++ android/modulator/src/syzygryd/modulator/Modulator.java
@@ -1,13 +1,19 @@
 package syzygryd.modulator;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class Modulator extends Activity
 {
    private static final String LOG = "SyzygyrdModulator";
+
+   private static final int MENU_SETTINGS = 0;
+   private static final int MENU_EXIT = 1;
 
    /* Activity */
    @Override
@@ -24,6 +30,38 @@ public class Modulator extends Activity
       // XXX will *not* calling super prevent the display from rotating ?
    }
 
+   /* Activity */
+   @Override
+   public boolean onCreateOptionsMenu(Menu menu) {
+      menu.add(0, MENU_SETTINGS, MENU_SETTINGS, "Settings");
+      menu.add(0, MENU_EXIT, MENU_EXIT, "Exit");
+      return true;
+   }
+   
+   /* Activity */
+   @Override
+   public boolean onOptionsItemSelected(MenuItem item) {
+      switch (item.getItemId())
+         {
+         case MENU_SETTINGS:
+            doSettings();
+            return true;
+         case MENU_EXIT:
+            doExit();
+            return true;
+         }
+      return false;
+   }
+
+   private void doSettings() {
+      Log.v(LOG, "Launching Activity for settings");
+      /* Context */ this.startActivity(new Intent(/* Context */ this, Preferences.class));
+   }
+   
+   private void doExit() {
+      Log.d(LOG, "Exitting");
+      /* Activity */ this.finish();
+   }
 }
 
 /*
