26370679f5058d049012ce654524daf83ed9ca31
Rich Fromm <noreply@ardentheavyindustries.com>
2010-08-18 17:57:52 +0000

    bug:57 i forgot to check this in before with svn r587 support 3 values in addition to up to 2 support int in addition to float this is getting to be horribly hacky, it really should be rewritten i also have no clue if by adding int i may have broken float.  totally untested.
    git-svn-id: http://www.ardentheavyindustries.com/svn/syzygryd/trunk@589 1c6c7da2-4a7c-0410-9345-fc53533f7dce

--- java/oscTools/SendOSC.java
+++ java/oscTools/SendOSC.java
@@ -24,18 +24,22 @@ public class SendOSC
       oscP5_ = new OscP5(this, port + 1);
    }
 
-   // XXX for now assume only floats
+   // XXX for now assume only floats and ints, this is hacky
 
    public void send(String address) {
-      send(address, null, null);
+      send(address, (Float)null, (Float)null, (Float)null);
    }
 
    public void send(String address, Float value1) {
-      send(address, value1, null);
+      send(address, value1, null, null);
    }
 
    public void send(String address, Float value1, Float value2) {
-      System.out.println("Sending OSC: " + address + " " + value1 + " " + value2);
+      send(address, value1, value2, null);
+   }
+
+   public void send(String address, Float value1, Float value2, Float value3) {
+      System.out.println("Sending OSC: " + address + " " + value1 + " " + value2 + " " + value3);
       OscMessage message = new OscMessage(address);
       if (value1 != null) {
          message.add(value1);
@@ -43,18 +47,45 @@ public class SendOSC
       if (value2 != null) {
          message.add(value2);
       }
+      if (value3 != null) {
+         message.add(value3);
+      }
+      oscP5_.send(message, oscSender_);
+   }
+
+   public void send(String address, Integer value1) {
+      send(address, value1, null, null);
+   }
+
+   public void send(String address, Integer value1, Integer value2) {
+      send(address, value1, value2, null);
+   }
+
+   public void send(String address, Integer value1, Integer value2, Integer value3) {
+      System.out.println("Sending OSC: " + address + " " + value1 + " " + value2 + " " + value3);
+      OscMessage message = new OscMessage(address);
+      if (value1 != null) {
+         message.add(value1);
+      }
+      if (value2 != null) {
+         message.add(value2);
+      }
+      if (value3 != null) {
+         message.add(value3);
+      }
       oscP5_.send(message, oscSender_);
    }
 
    // XXX not literal, given java classpath crap
    private static void usage() {
-      System.err.println("usage: SendOSC <host> <port> <address> [<value1> [<value2>]]");
+      System.err.println("usage: SendOSC <host> <port> <address> [<value1> [<value2> [<valie3>]]]");
       System.exit(1);
    }
 
    public static void main(String[] args) {
-      if (args.length >=3 && args.length <= 5) {
+      if (args.length >=3 && args.length <= 6) {
          try {
+            // assume float if not int
             String host = args[0];
             int port = Integer.parseInt(args[1]);
             String address = args[2];
@@ -62,16 +93,44 @@ public class SendOSC
             if (args.length == 3) {
                sendOSC.send(address);
             } else {
-               Float value1 = Float.parseFloat(args[3]);
+               Integer value1 = Integer.parseInt(args[3]);
                if (args.length == 4) {
                   sendOSC.send(address, value1);
                } else {
-                  Float value2 = Float.parseFloat(args[4]);
-                  sendOSC.send(address, value1, value2);
+                  Integer value2 = Integer.parseInt(args[4]);
+                  if (args.length == 5) {
+                     sendOSC.send(address, value1, value2);
+                  } else {
+                     Integer value3 = Integer.parseInt(args[5]);
+                     sendOSC.send(address, value1, value2, value3);
+                  }
                }
             }
          } catch (NumberFormatException nfe) {
-            usage();
+            try {
+               String host = args[0];
+               int port = Integer.parseInt(args[1]);
+               String address = args[2];
+               SendOSC sendOSC = new SendOSC(host, port);
+               if (args.length == 3) {
+                  sendOSC.send(address);
+               } else {
+                  Float value1 = Float.parseFloat(args[3]);
+                  if (args.length == 4) {
+                     sendOSC.send(address, value1);
+                  } else {
+                     Float value2 = Float.parseFloat(args[4]);
+                     if (args.length == 5) {
+                        sendOSC.send(address, value1, value2);
+                     } else {
+                        Float value3 = Float.parseFloat(args[5]);
+                        sendOSC.send(address, value1, value2, value3);
+                     }
+                  }
+               }
+            } catch (NumberFormatException nfe2) {
+               usage();
+            }
          }
       } else {
          usage();
