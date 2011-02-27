f0c4f4c19ecb84b8e0478d4e7d44edb7c4509a89
Rich Fromm <noreply@ardentheavyindustries.com>
2011-02-27 03:05:44 +0000

    execute applescript in a separate thread so that we can control the timeout the default timeout seems to be >90 seconds for now use 5 seconds when the higher level is rewritten, if we timeout here, we should probably throw an Exception here and catch it there, but that's not ready yet
    git-svn-id: http://www.ardentheavyindustries.com/svn/syzygryd/trunk@795 1c6c7da2-4a7c-0410-9345-fc53533f7dce

--- switcher/src/com/syzygryd/AppleScriptRunner.java
+++ switcher/src/com/syzygryd/AppleScriptRunner.java
@@ -4,6 +4,9 @@ import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Runs AppleScripts & stashes a reference to an AppleScript engine
  *
@@ -18,20 +21,74 @@ public class AppleScriptRunner {
 	
 	private static ScriptEngineManager mgr = new ScriptEngineManager();
 	private static ScriptEngine engine = mgr.getEngineByName("AppleScript");
+
+   private static final long APPLESCRIPT_TIMEOUT_MS = 5000;
+   // XXX for testing badness
+   //private static final long APPLESCRIPT_TIMEOUT_MS = 10;
+   
+   // XXX for testing only
+   // private static boolean debugOutput = false;
+   // private static Object debugObject = new Object();
     
 	/**
 	 * Runs a passed in script against stashed engine
 	 * @param script
 	 */
-    private static void runScript(String script) {
-    	try {
-			engine.eval(script);
-		} catch (ScriptException se) {
-			// TODO Auto-generated catch block
-         // XXX swallowing this is probably bad
-			Logger.warn(se);
-		}
-    }
+   private static void runScript(final String script) {
+      // synchronized (debugObject) {
+      //    if (!debugOutput) {
+      //       Logger.debug("GLOBAL_SCOPE = " + javax.script.ScriptContext.GLOBAL_SCOPE);
+      //       Logger.debug("ENGINE_SCOPE = " + javax.script.ScriptContext.ENGINE_SCOPE);
+      //       for (Integer scope: engine.getContext().getScopes()) {
+      //          Logger.debug("scope " + scope);
+      //          javax.script.Bindings bindings = engine.getContext().getBindings(scope.intValue());
+      //          for (String key: bindings.keySet()) {
+      //             Logger.debug("key: " + key);
+      //          }
+      //       }
+      //       debugOutput = true;
+      //    }
+      // }
+
+      final CountDownLatch scriptPending = new CountDownLatch(1);
+      // these are relatively infrequent enough that I won't worry about a thread pool
+      Thread t = new Thread(new Runnable() {
+            public void run() {
+               // The default timeout, which eventually throws the following,
+               // appears to be WAY too long (longer than 90 seconds), and I
+               // don't know of any way to set it.
+               //   javax.script.ScriptException: Live got an error: AppleEvent timed out.
+               // That's why we're running this in a separate thread
+               try {
+                  Logger.debug("Evaluating AppleScript: \"" + script + "\"");
+                  engine.eval(script);
+                  Logger.debug("Done evaluating AppleScript");
+                  scriptPending.countDown();
+               } catch (ScriptException se) {
+                  // TODO Auto-generated catch block
+                  // XXX swallowing this is probably bad
+                  Logger.warn(se);
+               }
+            }
+         });
+      Logger.debug("Starting background thread to execute script");
+      t.start();
+
+      Logger.debug("Will wait up to " + APPLESCRIPT_TIMEOUT_MS + " ms to execute script");
+      boolean scriptSuccessfullyExecuted = false;
+      try {
+         scriptSuccessfullyExecuted = scriptPending.await(APPLESCRIPT_TIMEOUT_MS, TimeUnit.MILLISECONDS);
+      } catch (InterruptedException ie) {
+      }
+
+      if (scriptSuccessfullyExecuted) {
+         Logger.debug("Script successfully executed");
+      } else {
+         Logger.warn("Script did not successfully execute in time");
+         // XXX we should probably throw an Exception here.  coming soon...
+         // Afaik, there's no way to cancel the script, but it's okay, b/c we'll probably be killing Live as a result of it
+      }
+   }
     
     /**
      * Quits live
