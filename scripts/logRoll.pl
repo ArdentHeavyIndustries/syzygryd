6d0f5c65da85028edde6c1d0573163d0e810c9fd
Rich Fromm <noreply@ardentheavyindustries.com>
2011-03-09 09:00:42 +0000

    add a log rolling script for now just a convenience to call by hand (i got tired of by hand rolling switcher logs on the syzyputer) but perhaps we should make this part of the startup sequence of various apps or hosts
    git-svn-id: http://www.ardentheavyindustries.com/svn/syzygryd/trunk@838 1c6c7da2-4a7c-0410-9345-fc53533f7dce

---
+++ scripts/logRoll.pl
@@ -0,0 +1,66 @@
+#!/usr/bin/env perl
+
+use warnings;
+use strict;
+use English;
+
+my $LOGDIR = "/opt/syzygryd/log";
+
+sub safeSystem($) {
+   my ($cmd) = @_;
+   chomp $cmd;
+   print $cmd, "\n";
+   my $returnValue = system($cmd);
+   if ($returnValue != 0) {
+      die "System call \"$cmd\" failed";
+   }
+}
+
+# early declaration necessary for recursion
+sub doRoll($$);
+
+sub doRoll($$) {
+   my ($logbase, $n) = @_;
+   my $src;
+   my $dest;
+
+   if ($n == 0) {
+      $src = $LOGDIR . '/' . $logbase . '.log';
+   } else {
+      $src = $LOGDIR . '/' . $logbase . '.' . $n . '.log';
+   }
+   if (!-e $src) {
+      return;
+   }
+
+   $dest = $LOGDIR . '/' . $logbase . '.' . ($n + 1) . '.log';
+   # if the dest file exists, first roll that by calling recursively
+   # XXX no recursion limit check implemented
+   if (-e $dest) {
+      doRoll($logbase, $n + 1);
+   }
+
+   safeSystem("mv $src $dest");
+}
+
+if ($#ARGV != 0) {
+   print "Rolls logs for a given program in $LOGDIR\n";
+   print "The <logBase> is the base name of the log files.\n";
+   print "For example, to roll all switcher logs (named switcher*log), use <logBase>=\"switcher\"\n";
+   print "\n";
+   print "usage: $PROGRAM_NAME <logBase>\n";
+   exit (0);
+} else {
+   my $logbase = $ARGV[0];
+   doRoll($logbase, 0);
+}
+
+##
+## Local Variables:
+##   mode: Perl
+##   perl-indent-level: 3
+##   indent-tabs-mode: nil
+## End:
+##
+## ex: set softtabstop=3 tabstop=3 expandtab sw=3:
+##
