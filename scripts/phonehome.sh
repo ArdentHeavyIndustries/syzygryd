11d8f3932c1b6af2a929d27792a9bd5e7497d616
Rich Fromm <noreply@ardentheavyindustries.com>
2011-03-12 20:37:04 +0000

    indent properly and reduce frequency from checking once every 3 seconds to every 15 seconds.
    git-svn-id: http://www.ardentheavyindustries.com/svn/syzygryd/trunk@856 1c6c7da2-4a7c-0410-9345-fc53533f7dce

--- scripts/phonehome.sh
+++ scripts/phonehome.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
-while true;
-do
-#echo "Checking for an existing session."
-PROCESS=`ps -A | grep "syzygryd@209.237.247.224" | grep -v grep`
-#echo $PROCESS
-if [ "$PROCESS" = "" ]
-then
-#echo "Attempting connection over local network."
-ssh -R 22210:localhost:22 -R 22211:10.10.10.11:22 -R 22212:10.10.10.12:22 -R 22213:10.10.10.13:22 syzygryd@209.237.247.224
-#echo "Local network connection timed out."
-fi
-#echo "Sleeping."
-sleep 3
+
+# copy this to /opt/syzygryd/bin/phonehome.sh on the syzyputer
+
+while true; do
+  #echo "Checking for an existing session."
+  PROCESS=`ps -A | grep "syzygryd@209.237.247.224" | grep -v grep`
+  #echo $PROCESS
+  if [ "$PROCESS" = "" ]; then
+    #echo "Attempting connection over local network."
+    ssh -R 22210:localhost:22 -R 22211:10.10.10.11:22 -R 22212:10.10.10.12:22 -R 22213:10.10.10.13:22 syzygryd@209.237.247.224
+    #echo "Local network connection timed out."
+  fi
+  #echo "Sleeping."
+  sleep 15
 done
+
+##
+## Local Variables:
+##   mode: Shell-script
+##   sh-basic-offset: 2
+##   indent-tabs-mode: nil
+## End:
+##
+## ex: set softtabstop=2 tabstop=2 expandtab:
+##
